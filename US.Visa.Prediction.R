c309b8ebbce9bbd3f51a827f66b3316f7282184c
unknown <danh.phan.mq@gmail.com>
2018-04-09 11:05:04 +1000

    Refine

--- US.Visa.Prediction.R
+++ US.Visa.Prediction.R
@@ -1,4 +1,5 @@
-
+## REFERENCES
+# Missing Data Analysis with mice: http://web.maths.unsw.edu.au/~dwarton/missingDataLab.html
 # Load data
 #library(readr)
 #us_perm_visas <- read_csv("D:/us_perm_visas.csv")
@@ -12,13 +13,13 @@ str(usvisa,list.len=200)
 # Check missing data
 library(mice)
 library(lattice)
-
+library(VIM)
 
 
 
 ### CHECK MISSING PARTTERNS FOR 30 FIRST VARIABLES
-usvisa_aggr <- aggr(usvisa[,1:30], col=mdc(1:2), numbers=TRUE, sortVars=TRUE, labels=names(usvisa[,1:30]), cex.axis=.7, gap=3, ylab=c("Proportion of missingness","Missingness Pattern"))
-usvisa_aggr <- aggr(usvisa[,1:20], col=mdc(1:2), numbers=TRUE, sortVars=TRUE, labels=names(usvisa[,1:20]), cex.axis=.6, gap=5, ylab=c("Proportion of missingness","Missingness Pattern"))
+usvisa_aggr <- aggr(usvisa[,1:30], col=mdc(1:2), numbers=TRUE, sortVars=TRUE, labels=names(usvisa[,1:30]), cex.axis=.6, gap=3, ylab=c("Proportion of missingness","Missingness Pattern"))
+#usvisa_aggr <- aggr(usvisa[,1:20], col=mdc(1:2), numbers=TRUE, sortVars=TRUE, labels=names(usvisa[,1:20]), cex.axis=.6, gap=5, ylab=c("Proportion of missingness","Missingness Pattern"))
 
 ### VARIABLES 31-60
 usvisa_aggr <- aggr(usvisa[,31:60], col=mdc(1:2), numbers=TRUE, sortVars=TRUE, labels=names(usvisa[,31:60]), cex.axis=.7, gap=3, ylab=c("Proportion of missingness","Missingness Pattern"))
@@ -29,20 +30,4 @@ usvisa_aggr <- aggr(usvisa[,91:120], col=mdc(1:2), numbers=TRUE, sortVars=TRUE, 
 ### VARIABLES 121-154
 usvisa_aggr <- aggr(usvisa[,121:150], col=mdc(1:2), numbers=TRUE, sortVars=TRUE, labels=names(usvisa[,121:150]), cex.axis=.7, gap=3, ylab=c("Proportion of missingness","Missingness Pattern"))
 
-str(usvisa)
-str(usvisa[,127:136])
-summary(usvisa[,127:136])
-md.pattern(usvisa[,127:136])
-# Missingness pattern can also be visualised in VIM package by
-library(VIM)
-usvisa_aggr <- aggr(usvisa[,127:136], col=mdc(1:2), numbers=TRUE, sortVars=TRUE, labels=names(usvisa[,127:136]), cex.axis=.7, gap=3, ylab=c("Proportion of missingness","Missingness Pattern"))
 
-usvisa_aggr <- aggr(usvisa[,127:136], col=mdc(1:2), numbers=TRUE, sortVars=TRUE, labels=names(usvisa[,127:136]), cex.axis=.7, gap=3, ylab=c("Proportion of missingness","Missingness Pattern"))
-
-# Next ten variables
-usvisa_aggr <- aggr(usvisa[,137:146], col=mdc(1:2), numbers=TRUE, sortVars=TRUE, labels=names(usvisa[,137:146]), cex.axis=.7, gap=3, ylab=c("Proportion of missingness","Missingness Pattern"))
-
-# Last eight variables
-usvisa_aggr <- aggr(usvisa[,147:154], col=mdc(1:2), numbers=TRUE, sortVars=TRUE, labels=names(usvisa[,147:154]), cex.axis=.7, gap=3, ylab=c("Proportion of missingness","Missingness Pattern"))
-summary(usvisa[,147:154])
-str(usvisa[,147:154])
