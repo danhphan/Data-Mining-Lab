9502cc99aaf7fdd6de25bb48d5103b9a9fd8fe5c
unknown <danh.phan.mq@gmail.com>
2018-04-12 11:27:25 +1000

    Note calculations

---
+++ Note.Clustering.R
@@ -0,0 +1,20 @@
+#######################################
+## Userful note for clustering
+#######################################
+
+
+
+##### HIARARCHICAL CLUSTERING ######
+
+# Create a random matrix
+
+
+##### K-MEANS CLUSTERING ######
+# K-means only works well with continuous and ordinal variables
+# Cause the distance calculation are meaningless with ordinal or factor variables.
+# It is recommended to use scaling/normalisation with original dataset before do K-means
+
+
+
+
+
