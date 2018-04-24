f38c96ef67f39cf0a45147a40e551a3ff7372d35
unknown <danh.phan.mq@gmail.com>
2018-04-24 22:49:29 +1000

    Perform linear discriminant analysis (LDA) for IRIS data

--- Bigdata.Analytics.R
+++ Bigdata.Analytics.R
@@ -26,5 +26,27 @@ dm.lm <- lm(log(price)~log(carat),data=diamonds)
 summary(dm.lm)
 
 # Test linear discriminant analysis (LDA)
+#install.packages("lda")
+library(lda)
+library(MASS)
+library(tibble)
+data("iris")
+summary(iris)
+# Convert to a tibble
+iris <- as.tibble(iris)
+summary(iris)
 
+ggplot(iris,aes(Sepal.Length,Sepal.Width,col=Species)) +
+  geom_point()
 
+# LDA model
+iris.lda <- lda(Species ~ Sepal.Length + Sepal.Width, data=iris)
+iris.lda
+
+sepal.grid <- expand.grid(Sepal.Length = seq(4,8,length=100),
+                          Sepal.Width = seq(2,4.5,length=100))
+
+sepal.grid <- as.tibble(sepal.grid)
+head(sepal.grid)
+sepal.grid$Species <- predict(iris.lda,sepal.grid)$class
+sepal.grid
