f099fbd1932d4df2d87cf7bcc6d7fb1022877b11
unknown <danh.phan.mq@gmail.com>
2018-04-25 00:39:13 +1000

    Test spark with dplyr commands.

--- Spark.R
+++ Spark.R
@@ -1,6 +1,6 @@
 
 # Spark in R
-
+library(ggplot2)
 library(dplyr)
 library(sparklyr)
 spark_install()
@@ -10,3 +10,25 @@ sc <- spark_connect(master = "local")
 library(nycflights13)
 flights_tbl <- copy_to(sc, nycflights13::flights, "flights")
 src_tbls(sc)
+# Filter with year of 2013
+flights_tbl %>% filter(year == 2013)
+
+flights_tbl %>% select(year,origin)
+
+flights_tbl %>%
+  group_by(origin) %>%
+  summarise(mean_delay = mean(dep_delay))
+flights_tbl %>% 
+  group_by(origin) %>% 
+  summarise(mean_delay = mean(dep_delay))
+
+delay <- flights_tbl %>% 
+  group_by(tailnum) %>%
+  summarise(count = n(), dist = mean(distance), delay = mean(arr_delay)) %>%
+  filter(count > 20, dist < 2000, !is.na(delay)) %>% 
+  collect
+
+ggplot(delay, aes(dist, delay)) +
+  geom_point(aes(size = count), alpha = 1/2) +
+  geom_smooth() +
+  scale_size_area(max_size = 2)
