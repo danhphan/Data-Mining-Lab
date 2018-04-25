d9c28029325f6aedf1fcd871658b76e5f7821ac9
unknown <danh.phan.mq@gmail.com>
2018-04-25 11:52:38 +1000

    Principal component analysis using sparklyr

--- Spark.R
+++ Spark.R
@@ -33,4 +33,55 @@ ggplot(delay, aes(dist, delay)) +
   geom_smooth() +
   scale_size_area(max_size = 2)
 
+flights_tbl
+
+# Convert to binary variable
+flights_tbl %>%
+  ft_binarizer("distance","long_flight",threshold = 1500) %>%
+  select(distance,long_flight)
+
+# Move to R again
+flights_tbl %>%
+  ft_binarizer("distance","long_flight",threshold = 1500) %>%
+  select(distance,long_flight) %>%
+  collect() %>%
+  mutate(long_flight2 = ifelse(long_flight == 0,"short","long"))
+
+# More categorical values?
+
+flights_tbl %>%
+  ft_bucketizer("distance","distance_cate",splits = c(0,500,1500,Inf)) %>%
+  select(distance,distance_cate)
+      
+
+###################################################
+## Principal component analysis using sparklyr
+###################################################
+
+iris_tbl <- copy_to(sc,iris,"iris",overwrite = TRUE)
+
+pca_model <- tbl(sc,"iris") %>%
+  select(-Species) %>%
+  ml_pca()
+print(pca_model)
+pca_model
+names(pca_model)
+
+# Matrix
+D <- as.matrix(iris[1:4])
+E <- as.matrix(pca_model$pc)
+P <- D %*% E
+
+PCs <- as.data.frame(P)
+PCs$Species <- iris$Species
+
+# Plot PCs
+
+ggplot(PCs,aes(PC1,PC2)) +
+  geom_point(aes(colour=Species))
+
+
+#######################################################
+## K-means clustering in sparklyr
+#######################################################
 
