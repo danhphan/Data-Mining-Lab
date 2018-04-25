d4e7af433d0a0add3c06bed4ca02b88ed25511f6
unknown <danh.phan.mq@gmail.com>
2018-04-25 15:31:44 +1000

    Plot the Neural network

--- Neural.Network.R
+++ Neural.Network.R
@@ -15,7 +15,11 @@ library(lattice)
 library(caret)
 
 # Train NN model
+# Using all predictors
 nn_model <- train(Species~.,iris,method="nnet",trace=FALSE)
+# Using 2 predictors
+#nn_model <- train(Species~Petal.Length+Petal.Width,iris,method="nnet",trace=FALSE)
+
 # Predict
 prediction <- predict(nn_model,iris)
 # Evaluate result
@@ -29,7 +33,7 @@ library(reshape)
 source_url('https://gist.githubusercontent.com/fawda123/7471137/raw/466c1474d0a505ff044412703516c34f1a4684a5/nnet_plot_update.r')
 plot.nnet(nn_model,nid=T,var.labs=F)
 
-###############################################
+#################################################
 ## How accurately can our network model the data?
 
 
